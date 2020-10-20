389c49197ca7609d3f02bf8c3df7b0d33e084c74
zermzhang <zhang371312@126.com>
2020-10-20 19:29:17 +0800

    [refactor]: refactor the implementation for activate AND deactivate function

--- src/extension.ts
+++ src/extension.ts
@@ -2,27 +2,20 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+function helloWorld(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "helloWorld" is now active!');
+	
 	let disposable = vscode.commands.registerCommand('helloworld.helloWorld', (uri) => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		// vscode.window.showInformationMessage('Hello VS Code!');
 		vscode.window.showInformationMessage(`the path for now is: ${uri ? uri.path: 'empty'}`)
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable)
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+exports.activate = helloWorld
+
+function deactivate() {
+
+}
+
+exports.deactivate = deactivate;
